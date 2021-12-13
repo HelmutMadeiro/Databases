6bf0f775cc85c49670dcf9dffef5cacfcaf1c469
Helmut Madeiro <HelmutMadeiro@users.noreply.github.com>
2021-12-12 21:21:05 -0300

    Create PostgreSQL 4.5 passos com sql EstruturaDeContrle.sql

---
+++ PostgreSQL/PostgreSQL 4.5 passos com sql EstruturaDeContrle.sql
@@ -0,0 +1,39 @@
+
+
+
+
+drop function salario_ok;
+
+create function salario_ok (instrutor instrutor) returns varchar as 
+$$
+	begin 
+		if instrutor.salario >200 then 
+			return 'Salario esta ok';
+		else 
+			return 'Salario pode aumentar';
+		end if;
+end;
+$$ language plpgsql;
+
+drop function salario_ok;
+
+create function salario_ok (id_instrutor integer ) returns varchar as 
+$$
+	declare
+		instrutor instrutor;
+	begin 
+		select * from instrutor where id = id_instrutor into instrutor;
+		if instrutor.salario >200 then 
+			return 'Salario esta ok';
+		else 
+			return 'Salario pode aumentar';
+		end if;
+end;
+$$ language plpgsql;
+
+select nome, salario_ok(instrutor.id) from instrutor;
+
+
+
+
+
