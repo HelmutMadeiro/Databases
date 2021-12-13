836c77a6bef094a10e38d8472e6219e37b4b9646
Helmut Madeiro <HelmutMadeiro@users.noreply.github.com>
2021-12-12 22:22:13 -0300

    Update PostgreSQL 4.5 passos com sql EstruturaDeContrle.sql

--- PostgreSQL/PostgreSQL 4.5 passos com sql EstruturaDeContrle.sql
+++ PostgreSQL/PostgreSQL 4.5 passos com sql EstruturaDeContrle.sql
@@ -17,23 +17,46 @@ $$ language plpgsql;
 
 drop function salario_ok;
 
-create function salario_ok (id_instrutor integer ) returns varchar as 
+create or replace function salario_ok (id_instrutor integer ) returns varchar as 
 $$
 	declare
 		instrutor instrutor;
 	begin 
 		select * from instrutor where id = id_instrutor into instrutor;
-		if instrutor.salario >200 then 
+		if instrutor.salario >300 then 
 			return 'Salario esta ok';
 		else 
-			return 'Salario pode aumentar';
+			 if instrutor.salario = 300 then
+				return 'Salario pode aumentar';
+			 else
+			 	return 'Salario esta defasado';
+			 end if;
 		end if;
-end;
+	end;
 $$ language plpgsql;
 
 select nome, salario_ok(instrutor.id) from instrutor;
 
 
+
+create or replace function salario_ok (id_instrutor integer ) returns varchar as 
+$$
+	declare
+		instrutor instrutor;
+	begin 
+		select * from instrutor where id = id_instrutor into instrutor;
+		if instrutor.salario >300 then 
+			return 'Salario esta ok';
+		elseif instrutor.salario = 300 then
+				return 'Salario pode aumentar';
+			 else
+			 	return 'Salario esta defasado';
+			 end if;
+		end if;
+	end;
+$$ language plpgsql;
+
+select nome, salario_ok(instrutor.id) from instrutor;
 
 
 
